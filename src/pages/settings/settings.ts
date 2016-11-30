90945cd8a2015940a908f35556d94e2bf75dde5a
Justin Willis <jgw9617@gmail.com>
2016-11-30 00:09:10 -0600

    settings and icon

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -17,10 +17,24 @@ export class SettingsPage {
   public notificationsValue: boolean;
   public vibrationsValue: boolean;
 
-  constructor(public navCtrl: NavController, public storage: Storage) {}
+  constructor(public navCtrl: NavController, public storage: Storage) { }
 
   ionViewDidLoad() {
     console.log('Hello SettingsPage Page');
+    this.storage.get('vibration').then((value) => {
+      if (value === null || value === true) {
+        this.vibrationsValue = true;
+      } else {
+        this.vibrationsValue = false;
+      }
+    });
+
+    let notifySetting = localStorage.getItem('notifications');
+    if (notifySetting === 'false' || notifySetting === null) {
+      this.notificationsValue = false;
+    } else {
+      this.notificationsValue = true;
+    }
   }
 
   public notifications() {
@@ -30,6 +44,13 @@ export class SettingsPage {
 
   public vibrations() {
     console.log(this.vibrationsValue);
+    if ('vibrate' in navigator) {
+      this.storage.get('vibration').then((value) => {
+        if (value === null || value === true) {
+          navigator.vibrate(300);
+        }
+      });
+    };
     this.storage.set('vibration', this.vibrationsValue);
   }
 
