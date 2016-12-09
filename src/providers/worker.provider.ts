08e90ddaa5b84b260af779dfa34563276a103c05
Justin Willis <jgw9617@gmail.com>
2016-12-08 19:35:48 -0600

    derp

--- src/providers/worker.provider.ts
+++ src/providers/worker.provider.ts
@@ -77,7 +77,7 @@ export class WorkerProvider {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
 
-    return this.http.post('https://whispering-ocean-77473.herokuapp.com//newId', endpoint, options)
+    return this.http.post('https://whispering-ocean-77473.herokuapp.com/newId', endpoint, options)
       .map(this.extractData)
       .catch(this.handleError)
   }
